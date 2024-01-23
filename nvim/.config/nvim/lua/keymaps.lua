d29fcdc9e8823257a9db1de1377ba62a2a75a597
dPenedo <dpenedo@tutanota.com>
2024-01-23 19:50:30 -0300

    orden y descripcion en castellano de todos los keymaps de neovim! Con chatGPT!

--- nvim/.config/nvim/lua/keymaps.lua
+++ nvim/.config/nvim/lua/keymaps.lua
@@ -1,19 +1,21 @@
--- TODO: Organizarlos con variables tipo lazyvim y agregar descripciones
--- See `:help vim.keymap.set()`
-vim.keymap.set({ 'n', 'v' }, '<Space>', '<Nop>', { silent = true })
+local function map(mode, key, value, options)
+  vim.keymap.set(mode, key, value, options or { silent = true })
+end
 
--- Remap for dealing with word wrap
-vim.keymap.set('n', 'k', "v:count == 0 ? 'gk' : 'k'", { expr = true, silent = true })
-vim.keymap.set('n', 'j', "v:count == 0 ? 'gj' : 'j'", { expr = true, silent = true })
+-- Mapeo para <Space> (espacio) usando vim.keymap
+map({ 'n', 'v' }, '<Space>', '<Nop>')
 
--- Diagnostic keymaps
-vim.keymap.set('n', '[d', vim.diagnostic.goto_prev, { desc = 'Go to previous diagnostic message' })
-vim.keymap.set('n', ']d', vim.diagnostic.goto_next, { desc = 'Go to next diagnostic message' })
-vim.keymap.set('n', '<leader>dd', vim.diagnostic.open_float, { desc = 'Open floating diagnostic message' })
-vim.keymap.set('n', '<leader>q', vim.diagnostic.setloclist, { desc = 'Open diagnostics list' })
+-- Remapeo para manejar el salto de palabras
+map('n', 'k', "v:count == 0 ? 'gk' : 'k'", { expr = true })
+map('n', 'j', "v:count == 0 ? 'gj' : 'j'", { expr = true })
 
--- [[ Highlight on yank ]]
--- See `:help vim.highlight.on_yank()`
+-- Mapeos para diagnósticos
+map('n', '[d', vim.diagnostic.goto_prev, { desc = 'Ir al mensaje de diagnóstico anterior' })
+map('n', ']d', vim.diagnostic.goto_next, { desc = 'Ir al siguiente mensaje de diagnóstico' })
+map('n', '<leader>dd', vim.diagnostic.open_float, { desc = 'Abrir mensaje de diagnóstico flotante' })
+map('n', '<leader>q', vim.diagnostic.setloclist, { desc = 'Abrir lista de diagnósticos' })
+
+-- Resaltado al pegar (yank)
 local highlight_group = vim.api.nvim_create_augroup('YankHighlight', { clear = true })
 vim.api.nvim_create_autocmd('TextYankPost', {
   callback = function()
@@ -23,76 +25,75 @@ vim.api.nvim_create_autocmd('TextYankPost', {
   pattern = '*',
 })
 
+-- Guardar rápidamente el búfer actual o todos los búferes
+map('n', '<A-s>', '<CMD>update<CR>', { desc = 'Guardar el búfer actual' })
+map('i', '<A-s>', '<CMD>update<CR>', { desc = 'Guardar el búfer actual' })
+map('v', '<A-s>', '<CMD>update<CR>', { desc = 'Guardar el búfer actual' })
+map('n', '<leader>W', '<CMD>wall<CR>', { desc = 'Guardar todos los búferes' })
 
--- Quickly save the current buffer or all buffers
-vim.keymap.set('n', '<A-s>', '<CMD>update<CR>')
-vim.keymap.set('i', '<A-s>', '<CMD>update<CR>')
-vim.keymap.set('v', '<A-s>', '<CMD>update<CR>')
-vim.keymap.set('n', '<leader>W', '<CMD>wall<CR>')
--- Mapeando el jj como Escape
-vim.keymap.set('i', 'jj', '<ESC>')
-vim.keymap.set('i', 'jk', '<ESC>la')
+-- Mapeos para salir del modo de inserción
+map('i', 'jj', '<ESC>', { desc = 'Salir del modo de inserción' })
+map('i', 'jk', '<ESC>la', { desc = 'Salir del modo de inserción y agregar una línea' })
 
--- Move to last buffer
-vim.keymap.set('n', "''", '<CMD>b#<CR>')
--- .setSet wrap!
-vim.keymap.set('n', '<a-w>', ':set wrap!<CR>')
--- .setNohl
-vim.keymap.set('n', '<Esc>', ':nohlsearch<CR>')
--- .setCopying the vscode behaviour of making tab splits
-vim.keymap.set('n', '<leader>\\', '<CMD>vsplit<CR>')
-vim.keymap.set('n', '<leader>|', '<CMD>split<CR>')
-vim.keymap.set('n', '<leader><CR>', '<CMD>vsplit<CR>')
-vim.keymap.set('n', '<leader>-', '<CMD>split<CR>')
+-- Moverse al último búfer
+map('n', "''", '<CMD>b#<CR>', { desc = 'Ir al último búfer' })
 
+-- Alternar ajuste de línea
+map('n', '<a-w>', ':set wrap!<CR>', { desc = 'Alternar ajuste de línea' })
 
--- Highlight colors
-vim.keymap.set('n', '<leader>cc', '<CMD>HighlightColorsToggle<CR>')
--- .setmap("n", "<A-->", "<CMD>split<CR>")
--- Filemanagers y terminales
--- vim.keymap.set('n', '<A-e>', ':Lf<CR>')
-vim.keymap.set('n', '<leader>e', ':Neotree position=float float<CR>')
-vim.keymap.set('n', '<A-o>', ':Oil<CR>')
-vim.keymap.set('n', '<leader>t', ':terminal<CR>:startinsert<CR>')
-vim.keymap.set('n', '<leader>E', ':NvimTreeToggle<CR>')
-vim.keymap.set('n', '<leader>Y', ':terminal yazi .<CR>:startinsert<CR>')
-vim.keymap.set('n', '<leader>E', ':terminal lf .<CR>:startinsert<CR>')
-vim.keymap.set("t", "<Esc>", "<C-\\><C-n><CR>")
-vim.keymap.set("t", "<C-x>", "<C-\\><C-n><CR>")
-vim.keymap.set("t", "<C-l>", "<C-u>clear<CR>")
+-- Limpiar resaltado de búsqueda
+map('n', '<Esc>', ':nohlsearch<CR>', { desc = 'Limpiar resaltado de búsqueda' })
 
+-- Dividir ventanas
+map('n', '<leader>\\', '<CMD>vsplit<CR>', { desc = 'Dividir verticalmente la ventana' })
+map('n', '<leader>|', '<CMD>split<CR>', { desc = 'Dividir horizontalmente la ventana' })
+map('n', '<leader><CR>', '<CMD>vsplit<CR>', { desc = 'Dividir verticalmente la ventana' })
+map('n', '<leader>-', '<CMD>split<CR>', { desc = 'Dividir horizontalmente la ventana' })
 
+-- Colores de resaltado
+map('n', '<leader>cc', '<CMD>HighlightColorsToggle<CR>', { desc = 'Alternar colores de resaltado' })
 
+-- Administradores de archivos y terminales
+map('n', '<leader>e', ':Neotree position=float float<CR>', { desc = 'Abrir administrador de archivos Neotree' })
+map('n', '<leader>E', ':NvimTreeToggle<CR>', { desc = 'Alternar visibilidad de Neotree' })
+map('n', '<leader>t', ':terminal<CR>:startinsert<CR>', { desc = 'Abrir terminal' })
+map('n', '<leader>Y', ':terminal yazi .<CR>:startinsert<CR>', { desc = 'Abrir terminal Yazi' })
+map('n', '<leader>E', ':terminal lf .<CR>:startinsert<CR>', { desc = 'Abrir terminal LF' })
 
--- setGestión de tabs/pestañas
-vim.keymap.set('n', '<leader>1', ':tabn 1<CR>')
-vim.keymap.set('n', '<leader>2', ':tabn 2<CR>')
-vim.keymap.set('n', '<leader>3', ':tabn 3<CR>')
-vim.keymap.set('n', '<leader>4', ':tabn 4<CR>')
-vim.keymap.set('n', '<leader>5', ':tabn 5<CR>')
-vim.keymap.set('n', '<leader>6', ':tabn 6<CR>')
-vim.keymap.set('n', '<leader>7', ':tabn 7<CR>')
-vim.keymap.set('n', '<leader>8', ':tabn 8<CR>')
-vim.keymap.set('n', '<leader>9', ':tabn 9<CR>')
--- .setcopiar/pegar
-vim.keymap.set('n', '<A-d>', '"_dd')
-vim.keymap.set('v', '<A-d>', '"_dd')
-vim.keymap.set('v', '<A-c>', '"+y')
-vim.keymap.set('v', '<leader>y', '"+y')
-vim.keymap.set('n', '<leader>y', '"+y')
+-- Mapeos para el terminal
+map("t", "<Esc>", "<C-\\><C-n><CR>", { desc = 'Salir del modo terminal' })
+map("t", "<C-x>", "<C-\\><C-n><CR>", { desc = 'Salir del modo terminal' })
+map("t", "<C-l>", "<C-u>clear<CR>", { desc = 'Limpiar la pantalla del terminal' })
 
-vim.keymap.set('n', '<C-h>', '<C-w>h')
-vim.keymap.set('n', '<C-j>', '<C-w>j')
-vim.keymap.set('n', '<C-k>', '<C-w>k')
-vim.keymap.set('n', '<C-l>', '<C-w>l')
-vim.keymap.set('n', '<C-right>', ':vertical resize +5<cr>')
-vim.keymap.set('n', '<C-left>', ':vertical resize -5<cr>')
-vim.keymap.set('n', '<C-down>', ':horizontal resize +2<cr>')
-vim.keymap.set('n', '<C-up>', ':horizontal resize -2<cr>')
+-- Gestión de pestañas
+map('n', '<leader>1', ':tabn 1<CR>', { desc = 'Ir a la pestaña 1' })
+map('n', '<leader>2', ':tabn 2<CR>', { desc = 'Ir a la pestaña 2' })
+map('n', '<leader>3', ':tabn 3<CR>', { desc = 'Ir a la pestaña 3' })
+map('n', '<leader>4', ':tabn 4<CR>', { desc = 'Ir a la pestaña 4' })
+map('n', '<leader>5', ':tabn 5<CR>', { desc = 'Ir a la pestaña 5' })
+map('n', '<leader>6', ':tabn 6<CR>', { desc = 'Ir a la pestaña 6' })
+map('n', '<leader>7', ':tabn 7<CR>', { desc = 'Ir a la pestaña 7' })
+map('n', '<leader>8', ':tabn 8<CR>', { desc = 'Ir a la pestaña 8' })
+map('n', '<leader>9', ':tabn 9<CR>', { desc = 'Ir a la pestaña 9' })
 
+-- Copiar/pegar
+map('n', '<A-d>', '"_dd', { desc = 'Eliminar línea sin afectar el registro' })
+map('v', '<A-d>', '"_dd', { desc = 'Eliminar línea sin afectar el registro' })
+map('v', '<A-c>', '"+y', { desc = 'Copiar al portapapeles' })
+map('v', '<leader>y', '"+y', { desc = 'Copiar al portapapeles' })
+map('n', '<leader>y', '"+y', { desc = 'Copiar al portapapeles' })
 
+-- Movimiento entre ventanas
+map('n', '<C-h>', '<C-w>h', { desc = 'Moverse a la ventana izquierda' })
 
--- recommended mappings
--- resizing splits
--- these keymaps will also accept a range,
--- for example `10<A-h>` will `resize_left` by `(10 * config.default_amount)`
+-- Mapeos para moverse entre ventanas
+map('n', '<C-h>', '<C-w>h', { desc = 'Moverse a la ventana izquierda' })
+map('n', '<C-j>', '<C-w>j', { desc = 'Moverse a la ventana inferior' })
+map('n', '<C-k>', '<C-w>k', { desc = 'Moverse a la ventana superior' })
+map('n', '<C-l>', '<C-w>l', { desc = 'Moverse a la ventana derecha' })
+
+-- Mapeos para redimensionar ventanas
+map('n', '<C-right>', ':vertical resize +5<CR>', { desc = 'Aumentar el tamaño de la ventana verticalmente' })
+map('n', '<C-left>', ':vertical resize -5<CR>', { desc = 'Reducir el tamaño de la ventana verticalmente' })
+map('n', '<C-down>', ':horizontal resize +2<CR>', { desc = 'Aumentar el tamaño de la ventana horizontalmente' })
+map('n', '<C-up>', ':horizontal resize -2<CR>', { desc = 'Reducir el tamaño de la ventana horizontalmente' })
