c9f71bffbb306e40fbc72f66c333973a36d32458
daniel <dpenedo@tutanota.com>
2024-02-11 18:42:22 -0300

    ✨ Gitcommit propio funcionando!

---
+++ scripts/scripts/commit.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# Ejecutar el script para obtener el emoji
+emoji=$(~/scripts/gitmoji-rofi.sh)
+
+# Solicitar entrada al usuario
+echo "Inserta el mensaje de commit:"
+read input_text
+
+# Concatenar el emoji y el texto ingresado por el usuario
+output="$emoji $input_text"
+
+# Mostrar el resultado
+echo "Tu commit: $output"
+
+git commit -m "$ouput"
