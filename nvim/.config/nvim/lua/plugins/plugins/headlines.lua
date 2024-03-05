14baa96f31799b1d29170046a0828f616cf20fae
daniel <dpenedo@tutanota.com>
2024-03-05 10:57:24 -0300

    Neorg eliminado y sustituido por headlines con markdown

---
+++ nvim/.config/nvim/lua/plugins/plugins/headlines.lua
@@ -0,0 +1,10 @@
+return   {
+        "lukas-reineke/headlines.nvim",
+        dependencies = "nvim-treesitter/nvim-treesitter",
+        opts = {
+                -- markdown = {
+                --                 headline_highlights = "Headline",
+                --
+                -- }
+        }
+    }
