799f7072e3c4c1d5650b6276d0411d7973f89ead
daniel <dpenedo@tutanota.com>
2024-05-26 21:51:32 -0300

    keymap neovim

--- nvim/.config/nvim/lua/keymaps.lua
+++ nvim/.config/nvim/lua/keymaps.lua
@@ -163,8 +163,8 @@ map('n', '<leader>9', ':tabn 9<CR>', { desc = 'Ir a la pestaña 9' })
 map('v', '<A-c>', '"+y', { desc = 'Copiar al portapapeles' })
 map('v', '<leader>y', '"+y', { desc = 'Copiar al portapapeles' })
 map('n', '<leader>y', '"+y', { desc = 'Copiar al portapapeles' })
-map('v', '<leader>p', ':FzfLua registers<cr>', { desc = 'Pegar con fzf lua' })
-map('n', '<leader>p', ':FzfLua registers<cr>', { desc = 'Pegar del portapapeles' })
+map('v', '<leader>p', ':Tele registers<cr>', { desc = 'Pegar con fzf lua' })
+map('n', '<leader>p', ':Tele registers<cr>', { desc = 'Pegar del portapapeles' })
 map('n', '<leader>v', 'ggVG', { desc = 'Seleccionar todo' })
 
 -- Scroll
