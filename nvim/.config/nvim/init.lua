a9fdccd70d81245205f7dd855cc2914cb4992a9c
daniel <dpenedo@tutanota.com>
2024-07-18 21:01:01 -0300

    tmux sin plugins

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -2,13 +2,22 @@ vim.g.mapleader = ' '
 vim.g.maplocalleader = ' '
 
 if vim.g.vscode then
-require 'vscode-keymaps'
-require 'vscode-settings'
+	require 'vscode-keymaps'
+	require 'vscode-settings'
 else
-require 'lazy-bootstrap'
-require 'plugins'
-require 'keymaps'
-require 'setups'
-require 'options'
-require 'color-mode'
+	require 'lazy-bootstrap'
+	require 'plugins'
+	require 'keymaps'
+	require 'setups'
+	require 'options'
+	require 'color-mode'
+
+
+
+	vim.cmd [[
+  augroup RestoreCursorShapeOnExit
+		autocmd!
+	  autocmd VimLeave * set guicursor=a:ver25-blinkon2000
+  augroup END
+]]
 end
