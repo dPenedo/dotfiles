32fb971a2f14ff51e63b84fe0c2b303b6a72e24a
daniel <dpenedo@tutanota.com>
2024-09-15 10:56:35 -0300

    algunos cambios en tmux y ene l inicio

--- scripts/scripts/inicio.sh
+++ scripts/scripts/inicio.sh
@@ -17,7 +17,7 @@ brave-browser -app=https://ticktick.com &
 brave-browser -app=https://www.meteored.com.ar/tiempo-en_Mar+del+Plata-America+Sur-Argentina-Provincia+de+Buenos+Aires-SAZM-1-16931.html &
 sleep 4
 kitty -e tmux &
-obsidian &
+# obsidian &
 setxkbmap -layout us -variant altgr-intl &
 # kitty -e bash ~/scripts/load_sessions.sh &
 sleep 2
