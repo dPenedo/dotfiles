af20a9cced0e8bbe823d1cb40cac3433dbb50384
daniel <dpenedo@tutanota.com>
2024-09-21 08:26:29 -0300

    telescope orden por reciente

--- nvim/.config/nvim/lua/setups/telescope-setup.lua
+++ nvim/.config/nvim/lua/setups/telescope-setup.lua
@@ -65,6 +65,12 @@ require('telescope').setup {
 			},
 		},
 	},
+	pickers = {
+		buffers = {
+			ignore_current_buffer = true,
+			sort_lastused = true,
+		},
+	},
 }
 
 -- Enable telescope fzf native, if installed
