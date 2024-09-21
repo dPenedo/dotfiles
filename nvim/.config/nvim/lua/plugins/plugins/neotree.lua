e592faedb6632cfff31d1c6ae56c6e7abf929f83
daniel <dpenedo@tutanota.com>
2024-09-21 08:10:56 -0300

    neotree como primario

--- nvim/.config/nvim/lua/plugins/plugins/neotree.lua
+++ nvim/.config/nvim/lua/plugins/plugins/neotree.lua
@@ -1,123 +1,141 @@
 return {
-  'nvim-neo-tree/neo-tree.nvim',
-  branch = 'v3.x',
-  dependencies = {
-    'nvim-lua/plenary.nvim',
-    'nvim-tree/nvim-web-devicons', -- not strictly required, but recommended
-    'MunifTanjim/nui.nvim',
-    -- 'mrbjarksen/neo-tree-diagnostics.nvim',
-    -- "3rd/image.nvim", -- Optional image support in preview window: See `# Preview Mode` for more information
-  },
-  event = 'VeryLazy',
-  config = function()
-    require('neo-tree').setup {
-      open_files_do_not_replace_types = { 'terminal', 'Trouble', 'qf', 'edgy' },
-      close_if_last_window = true, -- Close Neo-tree if it is the last window left in the tab
-      -- popup_border_style = { "▄", "▄", "▄", "█", "▀", "▀", "▀", "█" },
-      -- popup_border_style = "rounded",
-      enable_git_status = true,
-      enable_diagnostics = true,
-      sources = { 'filesystem', 'git_status' },
+	'nvim-neo-tree/neo-tree.nvim',
+	branch = 'v3.x',
+	dependencies = {
+		'nvim-lua/plenary.nvim',
+		'nvim-tree/nvim-web-devicons', -- not strictly required, but recommended
+		'MunifTanjim/nui.nvim',
+		-- 'mrbjarksen/neo-tree-diagnostics.nvim',
+		-- "3rd/image.nvim", -- Optional image support in preview window: See `# Preview Mode` for more information
+		{
+			's1n7ax/nvim-window-picker',
+			version = '2.*',
+			config = function()
+				require('window-picker').setup {
+					selection_chars = 'jkl;FJDKSLA;CMRUEIWOQP',
+					filter_rules = {
+						include_current_win = false,
+						autoselect_one = true,
+						-- filter using buffer options
+						bo = {
+							-- if the file type is one of following, the window will be ignored
+							filetype = { 'neo-tree', 'neo-tree-popup', 'notify' },
+							-- if the buffer type is one of following, the window will be ignored
+							buftype = { 'terminal', 'quickfix' },
+						},
+					},
+				}
+			end,
+		},
+	},
+	event = 'VeryLazy',
+	config = function()
+		require('neo-tree').setup {
+			open_files_do_not_replace_types = { 'terminal', 'Trouble', 'qf', 'edgy' },
+			close_if_last_window = true, -- Close Neo-tree if it is the last window left in the tab
+			-- popup_border_style = { "▄", "▄", "▄", "█", "▀", "▀", "▀", "█" },
+			-- popup_border_style = "rounded",
+			enable_git_status = true,
+			enable_diagnostics = true,
+			sources = { 'filesystem', 'git_status' },
 
-      window = {
-        position = 'right',
-        width = 50,
-        mapping_options = {
-          noremap = true,
-          nowait = true,
-        },
-        mappings = {
-          ['<space>'] = {
-            'toggle_node',
-            nowait = false, -- disable `nowait` if you have existing combos starting with this char that you want to use
-          },
-          ['<1-LeftMouse>'] = 'open',
-          ['<cr>'] = 'open',
-          ['l'] = 'open',
-          ['S'] = 'open_split',
-          ['<c-v>'] = 'open_vsplit',
-          -- ["S"] = "split_with_window_picker",
-          -- ["s"] = "vsplit_with_window_picker",
-          ['t'] = 'open_tabnew',
-          ['w'] = 'open_with_window_picker',
-          ['C'] = 'close_node',
-          ['a'] = {
-            'add',
-            -- some commands may take optional config options, see `:h neo-tree-mappings` for details
-            config = {
-              show_path = 'none', -- "none", "relative", "absolute"
-            },
-          },
-          ['A'] = 'add_directory', -- also accepts the config.show_path option.
-          ['d'] = 'delete',
-          ['r'] = 'rename',
-          ['y'] = 'copy_to_clipboard',
-          ['x'] = 'cut_to_clipboard',
-          ['p'] = 'paste_from_clipboard',
-          ['c'] = 'copy', -- takes text input for destination
-          ['m'] = 'move', -- takes text input for destination
-          ['q'] = 'close_window',
-          ['R'] = 'refresh',
-          ['?'] = 'show_help',
-        },
-      },
-      nesting_rules = {
-        -- ["js"] = { "js.map" },
-      },
-      filesystem = {
-        follow_current_file = {
-          enabled = true, -- This will find and focus the file in the active buffer every
-        },
-        -- time the current file is changed while the tree is open.
-        group_empty_dirs = false, -- when true, empty folders will be grouped together
-        hijack_netrw_behavior = 'open_default', -- netrw disabled, opening a directory opens neo-tree
-        -- in whatever position is specified in window.position
-        -- "open_current",  -- netrw disabled, opening a directory opens within the
-        -- window like netrw would, regardless of window.position
-        -- "disabled",    -- netrw left alone, neo-tree does not handle opening dirs
-        use_libuv_file_watcher = true, -- This will use the OS level file watchers to detect changes
-        -- instead of relying on nvim autocmd events.
-        window = {
-          mappings = {
-            ['H'] = 'navigate_up',
-            ['<bs>'] = 'toggle_hidden',
-            ['.'] = 'set_root',
-            ['f'] = 'filter_on_submit',
-            ['<c-x>'] = 'clear_filter',
-            ['[g'] = 'prev_git_modified',
-            [']g'] = 'next_git_modified',
-          },
-        },
-      },
-      buffers = {
-        follow_current_file = {
-          enabled = true, -- This will find and focus the file in the active buffer every
-        },
-        -- time the current file is changed while the tree is open.
-        group_empty_dirs = true, -- when true, empty folders will be grouped together
-        show_unloaded = true,
-        window = {
-          mappings = {
-            ['bd'] = 'buffer_delete',
-            ['<bs>'] = 'navigate_up',
-            ['.'] = 'set_root',
-          },
-        },
-      },
-      git_status = {
-        window = {
-          position = 'float',
-          mappings = {
-            ['A'] = 'git_add_all',
-            ['gu'] = 'git_unstage_file',
-            ['ga'] = 'git_add_file',
-            ['gr'] = 'git_revert_file',
-            ['gc'] = 'git_commit',
-            ['gp'] = 'git_push',
-            ['gg'] = 'git_commit_and_push',
-          },
-        },
-      },
-    }
-  end,
+			window = {
+				position = 'right',
+				mapping_options = {
+					noremap = true,
+					nowait = true,
+				},
+				mappings = {
+					['<space>'] = {
+						'toggle_node',
+						nowait = false, -- disable `nowait` if you have existing combos starting with this char that you want to use
+					},
+					['<1-LeftMouse>'] = 'open',
+					['<cr>'] = 'open',
+					['l'] = 'open',
+					['S'] = 'open_split',
+					['<c-v>'] = 'open_vsplit',
+					-- ["S"] = "split_with_window_picker",
+					-- ["s"] = "vsplit_with_window_picker",
+					['t'] = 'open_tabnew',
+					['w'] = 'open_with_window_picker',
+					['C'] = 'close_node',
+					['a'] = {
+						'add',
+						-- some commands may take optional config options, see `:h neo-tree-mappings` for details
+						config = {
+							show_path = 'none', -- "none", "relative", "absolute"
+						},
+					},
+					['A'] = 'add_directory', -- also accepts the config.show_path option.
+					['d'] = 'delete',
+					['r'] = 'rename',
+					['y'] = 'copy_to_clipboard',
+					['x'] = 'cut_to_clipboard',
+					['p'] = 'paste_from_clipboard',
+					['c'] = 'copy', -- takes text input for destination
+					['m'] = 'move', -- takes text input for destination
+					['q'] = 'close_window',
+					['R'] = 'refresh',
+					['?'] = 'show_help',
+				},
+			},
+			nesting_rules = {
+				-- ["js"] = { "js.map" },
+			},
+			filesystem = {
+				follow_current_file = {
+					enabled = true, -- This will find and focus the file in the active buffer every
+				},
+				-- time the current file is changed while the tree is open.
+				group_empty_dirs = false,       -- when true, empty folders will be grouped together
+				hijack_netrw_behavior = 'open_default', -- netrw disabled, opening a directory opens neo-tree
+				-- in whatever position is specified in window.position
+				-- "open_current",  -- netrw disabled, opening a directory opens within the
+				-- window like netrw would, regardless of window.position
+				-- "disabled",    -- netrw left alone, neo-tree does not handle opening dirs
+				use_libuv_file_watcher = true, -- This will use the OS level file watchers to detect changes
+				-- instead of relying on nvim autocmd events.
+				window = {
+					mappings = {
+						['H'] = 'navigate_up',
+						['.'] = 'toggle_hidden',
+						['f'] = 'filter_on_submit',
+						['<c-x>'] = 'clear_filter',
+						['[g'] = 'prev_git_modified',
+						[']g'] = 'next_git_modified',
+					},
+				},
+			},
+			buffers = {
+				follow_current_file = {
+					enabled = true, -- This will find and focus the file in the active buffer every
+				},
+				-- time the current file is changed while the tree is open.
+				group_empty_dirs = true, -- when true, empty folders will be grouped together
+				show_unloaded = true,
+				window = {
+					mappings = {
+						['bd'] = 'buffer_delete',
+						['<bs>'] = 'navigate_up',
+						['.'] = 'set_root',
+					},
+				},
+			},
+			git_status = {
+				window = {
+					position = 'float',
+					mappings = {
+						['A'] = 'git_add_all',
+						['gu'] = 'git_unstage_file',
+						['ga'] = 'git_add_file',
+						['gr'] = 'git_revert_file',
+						['gc'] = 'git_commit',
+						['gp'] = 'git_push',
+						['gg'] = 'git_commit_and_push',
+					},
+				},
+			},
+		}
+	end,
 }
