4ca02340e9710f7fbde6af3e959070d9b9f312dc
daniel <dpenedo@tutanota.com>
2024-10-07 20:44:23 -0300

    snippets python y javascript

--- nvim/.config/nvim/lua/setups/snippets/snippets-javascript.lua
+++ nvim/.config/nvim/lua/setups/snippets/snippets-javascript.lua
@@ -6,65 +6,89 @@ local func = ls.function_node
 
 -- Función para extraer y formatear el nombre de archivo
 local filename = function()
-	local name = vim.fn.expand '%:t'
-	name = name:gsub('%.js$', '')       -- Remover extensión .js
-	name = name:gsub('%.jsx$', '')      -- Remover extensión .jsx
-	name = name:gsub('_', ' ')          -- Reemplazar guiones bajos con espacios
-	name = name:gsub('-', ' ')          -- Reemplazar guiones con espacios
-	name = name:gsub('^%l', string.upper) -- Capitalizar la primera letra
-	return { name }
+  local name = vim.fn.expand '%:t'
+  name = name:gsub('%.js$', '') -- Remover extensión .js
+  name = name:gsub('%.jsx$', '') -- Remover extensión .jsx
+  name = name:gsub('_', ' ') -- Reemplazar guiones bajos con espacios
+  name = name:gsub('-', ' ') -- Reemplazar guiones con espacios
+  name = name:gsub('^%l', string.upper) -- Capitalizar la primera letra
+  return { name }
 end
 
 -- Snippets compartidos
 local snippets_js = {
-	-- className=""
-	snip({
-		trig = 'className',
-		namr = 'className',
-		dscr = 'Insert className for React',
-	}, {
-		text 'className="',
-		insert(1),
-		text '"',
-	}),
-	-- Comment on JSX
-	snip({
-		namr = 'commentJSX',
-		trig = 'commentJSX',
-		dscr = 'Comment on JSX',
-	}, {
-		text '{/* ',
-		insert(1),
-		text ' */}',
-	}),
-	-- Empty tag on JSX
-	snip({
-		namr = '<></>',
-		trig = '<>',
-		dscr = 'Empty tag  on JSX',
-	}, {
-		text '<>',
-		insert(1),
-		text '',
-		text '</>',
-	}),
-	-- Arrow function
-	-- Component with export
-	snip({
-		trig = 'rfc',
-		namr = 'React Functional Component',
-		dscr = 'Create a React functional component',
-	}, {
-		text 'export const ',
-		func(filename, {}),
-		text ' = () => {',
-		text '\n  return (',
-		text '\n    <div>',
-		insert(1),
-		text '</div>',
-		text '\n  );',
-		text '\n};',
-	}),
+  -- className=""
+  snip({
+    trig = 'className',
+    namr = 'className',
+    dscr = 'Insert className for React',
+  }, {
+    text 'className="',
+    insert(1),
+    text '"',
+  }),
+  -- Comment on JSX
+  snip({
+    namr = 'commentJSX',
+    trig = 'commentJSX',
+    dscr = 'Comment on JSX',
+  }, {
+    text '{/* ',
+    insert(1),
+    text ' */}',
+  }),
+  -- Empty tag on JSX
+  snip({
+    namr = '<></>',
+    trig = '<>',
+    dscr = 'Empty tag  on JSX',
+  }, {
+    text '<>',
+    insert(1),
+    text '',
+    text '</>',
+  }),
+  -- Arrow function
+  -- Component with export
+  snip({
+    trig = 'rfc',
+    namr = 'React Functional Component',
+    dscr = 'Create a React functional component',
+  }, {
+    text 'export const ',
+    func(filename, {}),
+    text ' = () => {',
+    text '\n  return (',
+    text '\n    <div>',
+    insert(1),
+    text '</div>',
+    text '\n  );',
+    text '\n};',
+  }),
+  -- TODO
+  snip({
+    trig = 'todo',
+    namr = 'TODO comment',
+    dscr = 'Insert a TODO comment',
+  }, {
+    text '// TODO: ',
+  }),
+  -- WARN
+  snip({
+    trig = 'warn',
+    namr = 'WARN comment',
+    dscr = 'Insert a WARN comment',
+  }, {
+    text '// WARN: ',
+  }),
+  -- NOTE
+  snip({
+    trig = 'note',
+    namr = 'NOTE comment',
+    dscr = 'Insert a NOTE comment',
+  }, {
+    text '// NOTE: ',
+  }),
 }
 
 -- Agregar los snippets para 'javascript' y 'javascriptreact'
--- nvim/.config/nvim/lua/setups/snippets/snippets-python.lua
+++ nvim/.config/nvim/lua/setups/snippets/snippets-python.lua
@@ -8,46 +8,62 @@ local func = ls.function_node
 local choice = ls.choice_node
 
 local filename = function()
-	local name = vim.fn.expand '%:t'
-	name = name:gsub('%.py$', '') -- Remove .py extension
-	name = name:gsub('_', ' ')  -- Replace underscores with spaces
-	name = name:gsub('-', ' ')  -- Replace hyphens with spaces
-	-- Capitalize the first letter
-	name = name:gsub('^%l', string.upper)
-	return { name }
+  local name = vim.fn.expand '%:t'
+  name = name:gsub('%.py$', '') -- Remove .py extension
+  name = name:gsub('_', ' ') -- Replace underscores with spaces
+  name = name:gsub('-', ' ') -- Replace hyphens with spaces
+  -- Capitalize the first letter
+  name = name:gsub('^%l', string.upper)
+  return { name }
 end
 local date = function()
-	return { os.date '%d-%m-%Y' }
+  return { os.date '%d-%m-%Y' }
 end
 
 ls.add_snippets('python', {
-	-- print()
-	snip({
-		trig = 'print',
-		namr = 'Print Statement',
-		dscr = 'Insert a print function',
-	}, {
-		text 'print(',
-		insert(1),
-		text ')',
-	}),
-	-- type()
-	snip({
-		trig = 'type',
-		namr = 'type Statement',
-		dscr = 'Insert a type function',
-	}, {
-		text 'type(',
-		insert(1),
-		text ')',
-	}),
-	-- TODO
-	snip({
-		trig = 'todo',
-		namr = 'TODO comment',
-		dscr = 'Insert a TODO comment',
-	}, {
-		text '# TODO: ',
-	}),
-	-- Add more Python snippets here
+  -- print()
+  snip({
+    trig = 'print',
+    namr = 'Print Statement',
+    dscr = 'Insert a print function',
+  }, {
+    text 'print(',
+    insert(1),
+    text ')',
+  }),
+  -- type()
+  snip({
+    trig = 'type',
+    namr = 'type Statement',
+    dscr = 'Insert a type function',
+  }, {
+    text 'type(',
+    insert(1),
+    text ')',
+  }),
+  -- TODO
+  snip({
+    trig = 'todo',
+    namr = 'TODO comment',
+    dscr = 'Insert a TODO comment',
+  }, {
+    text '# TODO: ',
+  }),
+  -- WARN
+  snip({
+    trig = 'warn',
+    namr = 'WARN comment',
+    dscr = 'Insert a WARN comment',
+  }, {
+    text = '# WARN: ',
+  }),
+  -- NOTE
+  snip({
+    trig = 'note',
+    namr = 'NOTE comment',
+    dscr = 'Insert a NOTE comment',
+  }, {
+    text '#  NOTE: ',
+  }),
+  -- Add more Python snippets here
 })
