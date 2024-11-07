7802a6c2dd1d454cc2bf3207e29e9cd2e5fe12d7
daniel <dpenedo@tutanota.com>
2024-11-07 10:37:50 -0300

    neotree configurado

--- nvim/.config/nvim/lua/plugins/disabled.lua
+++ nvim/.config/nvim/lua/plugins/disabled.lua
@@ -1,7 +1,7 @@
 return {
   -- disable trouble
   -- { "akinsho/bufferline.nvim", enabled = false },
-  { "nvim-neo-tree/neo-tree.nvim", enabled = false },
+  { "nvim-neo-tree/neo-tree.nvim", enabled = true },
   -- { "folke/noice.nvim", enabled = false },
   -- { "folke/flash.nvim", enabled = false },
   -- { "nvim-lualine/lualine.nvim", enabled = false },
